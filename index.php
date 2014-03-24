07e109627aa47a9e03828bda1daa67d97a766d2d
Stephen Welch <stephencwelch@gmail.com>
2014-03-24 10:53:40 -0400

    copied bootstrap template

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
+
+
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="-/js/bootstrap.js"></script>
+    <script src="-/js/myscript.js"></script>
+  </body>
+</html>
