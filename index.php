5160163a743a588508c3be7587fc8b14f75b862f
Stephen Welch <stephencwelch@gmail.com>
2014-03-24 15:31:34 -0400

    Adding Background, video, nav, brief details

--- index.php
+++ index.php
@@ -12,11 +12,183 @@
 
 
   </head>
-  <body>
-    <h1>Hello, world!</h1>
+
+  <nav class="navbar navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">
+          <img src="images/logo.png" alt = "meh"> 
+        </a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="active"><a href="#">
+            <span class="glyphicon glyphicon-signal"></span>
+            SAT</a></li>
+          <li><a href="#">
+            <span class="glyphicon glyphicon-star"></span>
+            Recommendations</a></li>
+          <li><a href="#">
+            <span class="glyphicon glyphicon-facetime-video"></span>
+            Videos</a></li>
+          <li><a href="#">
+            <span class="glyphicon glyphicon-bookmark"></span>
+            Private Tutoring</a></li>
+          <li><a href="#">
+            <span class="glyphicon glyphicon-user"></span>
+            About</a></li>
+
+          <li>
+            <button type="button" class="btn btn-success navbar-btn">Sign Up</button>
+          </li>
+
+          <li> <a href="#"> </a></li> <!-- Little Hack here to make some space -->
+        </ul>
+
+        
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>  
+
+  <!--parallax 1 -->
+  <section class="bg-1 text-center">
+    <div class="col-md-7 col-md-offset-1">
+        <div class="contentLeft">
+          <div class="flex-video widescreen"><iframe width="640" height="360" src="//www.youtube.com/embed/gCFxmfSwecc?controls=2&showinfo=0" frameborder="0" allowfullscreen></iframe></div>
+
+       </div>
+      </div>
+
+  </section>
+  <!--/parallax 1-->
+
+
+  <div class="container">
+    
+      <hr>
+    
+      <div class="row">
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-heading"><h3>
+                 <span class="glyphicon glyphicon-ok"></span>
+                Proven Results</h3></div>
+              <div class="panel-body">
+
+                I begin the SAT prep process by working with students to select several target universities. 
+                From there, we determine our attack plan and target scores. 
+                Everything from then on, down to the number of questions a student 
+                will answer on a specific portion of the test, is determined by their 
+                target score. I have worked with students with broad ranges of target 
+                scores, from 1200 all the way to 2300+, and helped students make score 
+                improvements of 100 to over 400 points. 
+              </div>
+              <div class="panel-footer text-right"><a href="#" class="btn btn-default">More</a></div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-heading"><h3>
+                <span class="glyphicon glyphicon-pencil"></span>
+                Highly individualized instruction</h3></div>
+              <div class="panel-body">I believe all students have the ability to excel academically. The truth is, it's almost impossible for a single teacher to provide instruction that keeps students in the critical zone where learning really happens. Students are too often either behind and lost or ahead and bored. I make use of small class sizes plus a wide range of teaching tools to keep students in the critical learning zone. Classes consist of lecturing, exploratory problem solving, videos, online resources, visualizations, written examples, team assignments, peer-teaching, and even games.
+              </div>
+              <div class="panel-footer text-right"><a href="#" class="btn btn-default">More</a></div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-heading"><h3>
+                <span class="glyphicon glyphicon-user"></span>
+                Class Sized of Six or Less</h3></div>
+              <div class="panel-body">Small classes allow for significant one-on-one time, individualized instruction, and the benefit of interacting with other learners. 
+              </div>
+              <div class="panel-footer text-right"><a href="#" class="btn btn-default">More</a></div>
+            </div>
+          </div>
+      </div><!--/row-->
+    
+      <hr>
+    
+  </div><!--/container-->
+
+  <div class="divider"></div>
+    
+  <section class="bg-2">
+    <div class="col-sm-10 col-sm-offset-1 text-center">
+    
+      <h1>Keep Going</h1>
+      
+    </div>
+  </section>
+
+  <div class="divider"></div>
+
+  <div class="container">
+    
+      <div class="row">
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-heading"><h3>Hello.</h3></div>
+              <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+              Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+              dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+              Aliquam in felis sit amet augue.
+              </div>
+              <div class="panel-footer text-right"><a href="#" class="btn btn-default">More</a></div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-heading"><h3>Hello.</h3></div>
+              <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+              Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+              dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+              Aliquam in felis sit amet augue.
+              </div>
+              <div class="panel-footer text-right"><a href="#" class="btn btn-default">More</a></div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-heading"><h3>Hello.</h3></div>
+              <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+              Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+              dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+              Aliquam in felis sit amet augue.
+              </div>
+              <div class="panel-footer text-right"><a href="#" class="btn btn-default">More</a></div>
+            </div>
+          </div>
+      </div>  
+  </div><!--/container-->
+
+  <div class="divider"></div>
+
+  <!--parallax 2 -->
+  <section class="bg-3 text-center">
+    <h1>Bootstrap Parallax</h1>
+    <p class="lead"></p>
+  </section>
+  <!--/parallax 2-->
+
+  <div id="footer">
+    <div class="container">
+      <p class="text-muted">This Bootstrap Example courtesy <a href="http://www.bootply.com">Bootply.com</a></p>
+    </div>
+  </div>
 
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="-/js/bootstrap.js"></script>
-    <script src="-/js/myscript.js"></script>
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscript.js"></script>
   </body>
 </html>
